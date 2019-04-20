9fb91b10fbf2f373130fdad7f340f9528e745920
Zach Panzarino <zachary@panzarino.com>
2019-04-20 17:49:33 -0400

    Start on login page setup

--- index.php
+++ index.php
@@ -3,13 +3,30 @@
 <!-- Page Content -->
 <div class="container">
     <div class="row">
-        <div class="col-lg-12 text-center">
-            <h1 class="mt-5">A Bootstrap 4 Starter Template</h1>
-            <p class="lead">Complete with pre-defined file paths and responsive navigation!</p>
-            <ul class="list-unstyled">
-                <li>Bootstrap 4.2.1</li>
-                <li>jQuery 3.3.1</li>
-            </ul>
+        <div class="col-lg-6 offset-lg-3 text-center">
+            <h1 class="mt-5">Atlanta Beltline Login</h1>
+            <form>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input type="email" class="form-control" name="email">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Password</label>
+                    <div class="col-sm-10">
+                        <input type="password" class="form-control" name="password">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <div class="col-sm-6 text-center">
+                        <button type="submit" class="btn btn-primary">Log In</button>
+                    </div>
+                    <div class="col-sm-6 text-center">
+                        <a href="" class="btn btn-primary">Register</a>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
