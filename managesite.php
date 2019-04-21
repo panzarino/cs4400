35c8b167652d00838ff29dbbd2ed1cfd7580a8c4
Zach Panzarino <zachary@panzarino.com>
2019-04-21 11:32:22 -0400

    Create manage site page

--- managesite.php
+++ managesite.php
@@ -11,14 +11,25 @@ $connection = mysqli_connect(
     $_SERVER['DB_DATABASE']
 );
 
-$users = [];
-$query = mysqli_prepare($connection, "SELECT 'visitor' AS Type, User.Username, Status, (SELECT COUNT(*) FROM UserEmail WHERE UserEmail.Username = User.Username) AS EmailCount FROM User JOIN Visitor ON User.Username=Visitor.Username WHERE User.Username NOT IN (SELECT Username FROM Employee) UNION SELECT 'staff' AS Type, User.Username, Status, (SELECT COUNT(*) FROM UserEmail WHERE UserEmail.Username = User.Username) AS EmailCount FROM User JOIN Staff ON User.Username=Staff.Username UNION SELECT 'manager' AS Type, User.Username, Status, (SELECT COUNT(*) FROM UserEmail WHERE UserEmail.Username = User.Username) AS EmailCount FROM User JOIN Manager ON User.Username=Manager.Username UNION SELECT 'user' AS Type, User.Username, Status, (SELECT COUNT(*) FROM UserEmail WHERE UserEmail.Username = User.Username) AS EmailCount FROM User WHERE Username NOT IN (SELECT Username FROM Employee UNION SELECT Username FROM Visitor)");
+$sites = [];
+$query = mysqli_prepare($connection, "SELECT SiteName, OpenEveryday, Username, Firstname, Lastname FROM Site JOIN User ON Site.ManagerUsername=User.Username");
 mysqli_stmt_execute($query);
-mysqli_stmt_bind_result($query, $resulttype, $resultusername, $resultstatus, $resultemailcount);
+mysqli_stmt_bind_result($query, $resultname, $resultopen, $resultusername, $resultfirstname, $resultlastname);
 while (mysqli_stmt_fetch($query)) {
-    array_push($users, array('type' => $resulttype, 'username' => $resultusername, 'status' => $resultstatus, 'emailCount' => $resultemailcount));
+    array_push($sites, array('name' => $resultname, 'open' => $resultopen, 'username' => $resultusername, 'firstName' => $resultfirstname, 'lastName' => $resultlastname));
 }
 mysqli_stmt_close($query);
+
+$managers = [];
+$query = mysqli_prepare($connection, "SELECT User.Username, User.Firstname, User.Lastname FROM Manager JOIN User ON Manager.Username=User.Username");
+mysqli_stmt_execute($query);
+mysqli_stmt_bind_result($query, $resultusername, $resultfirstname, $resultlastname);
+while (mysqli_stmt_fetch($query)) {
+    array_push($managers, array('username' => $resultusername, 'firstName' => $resultfirstname, 'lastName' => $resultlastname));
+}
+mysqli_stmt_close($query);
+
+mysqli_close($connection);
 ?>
 
 <?php include('header.php') ?>
@@ -27,54 +38,69 @@ mysqli_stmt_close($query);
 <div class="container">
     <div class="row">
         <div class="col-lg-8 offset-lg-2 text-center">
-            <h1 class="mt-5">Manage User</h1>
-            <form action="manageuser.php" method="GET">
-<!--                <div class="row">-->
-<!--                    <div class="col-md-6">-->
-<!--                        <div class="form-group row">-->
-<!--                            <label class="col-sm-4 col-form-label">Username</label>-->
-<!--                            <div class="col-sm-8">-->
-<!--                                <input type="text" class="form-control" name="username" value="--><?//= $username ?><!--">-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                    <div class="col-md-6">-->
-<!--                        <div class="form-group row">-->
-<!--                            <label class="col-sm-4 col-form-label">Type</label>-->
-<!--                            <div class="col-sm-8">-->
-<!--                                <select name="type" class="form-control" required>-->
-<!--                                    <option value="all" --><?//= $type == 'all' || $type == null ? 'selected' : '';?><!-->All</option>-->
-<!--                                    <option value="user" --><?//= $type == 'user' ? 'selected' : '';?><!-->User</option>-->
-<!--                                    <option value="visitor" --><?//= $type == 'visitor' ? 'selected' : '';?><!-->Visitor</option>-->
-<!--                                    <option value="staff" --><?//= $type == 'staff' ? 'selected' : '';?><!-->Staff</option>-->
-<!--                                    <option value="manager" --><?//= $type == 'manager' ? 'selected' : '';?><!-->Manager</option>-->
-<!--                                </select>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
-<!--                <div class="row">-->
-<!--                    <div class="col-md-6">-->
-<!--                        <div class="form-group row">-->
-<!--                            <label class="col-sm-4 col-form-label">Status</label>-->
-<!--                            <div class="col-sm-8">-->
-<!--                                <select name="status" class="form-control" required>-->
-<!--                                    <option value="all" --><?//= $status == 'all' || $status == null ? 'selected' : '';?><!-->All</option>-->
-<!--                                    <option value="approved" --><?//= $status == 'approved' ? 'selected' : '';?><!-->Approved</option>-->
-<!--                                    <option value="pending" --><?//= $status == 'pending' ? 'selected' : '';?><!-->Pending</option>-->
-<!--                                    <option value="declined" --><?//= $status == 'declined' ? 'selected' : '';?><!-->Declined</option>-->
-<!--                                </select>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                    <div class="col-md-6">-->
-<!--                        <div class="form-group row">-->
-<!--                            <div class="col-sm-12 text-center">-->
-<!--                                <button type="submit" class="btn btn-primary">Filter</button>-->
-<!--                            </div>-->
-<!--                        </div>-->
-<!--                    </div>-->
-<!--                </div>-->
+            <h1 class="mt-5">Manage Site</h1>
+            <form action="managesite.php" method="GET">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Site</label>
+                            <div class="col-sm-8">
+                                <select name="site" class="form-control" required>
+                                    <option value="all" <?= $site == 'all' || $site == null ? 'selected' : '';?>>All</option>
+                                    <?php
+                                    foreach ($sites as $s) {
+                                        if ($site == $s['name']) {
+                                            echo '<option value="'.$s['name'].'" selected>'.$s['name'].'</option>';
+                                        } else {
+                                            echo '<option value="' . $s['name'] . '">' . $s['name'] . '</option>';
+                                        }
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Manager</label>
+                            <div class="col-sm-8">
+                                <select name="manager" class="form-control" required>
+                                    <option value="all" <?= $manager == 'all' || $manager == null ? 'selected' : '';?>>All</option>
+                                    <?php
+                                    foreach ($managers as $m) {
+                                        if ($manager == $m['username']) {
+                                            echo '<option value="'.$m['username'].'" selected>'.$m['firstName'].' '.$m['lastName'].'</option>';
+                                        } else {
+                                            echo '<option value="'.$m['username'].'">'.$m['firstName'].' '.$m['lastName'].'</option>';
+                                        }
+                                    }
+                                    ?>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group row">
+                            <label class="col-sm-4 col-form-label">Open Everyday</label>
+                            <div class="col-sm-8">
+                                <select name="open" class="form-control" required>
+                                    <option value="all" <?= $open == 'all' || $open == null ? 'selected' : '';?>>All</option>
+                                    <option value="yes" <?= $open == 'yes' ? 'selected' : '';?>>Yes</option>
+                                    <option value="no" <?= $open == 'no' ? 'selected' : '';?>>No</option>
+                                </select>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group row">
+                            <div class="col-sm-12 text-center">
+                                <button type="submit" class="btn btn-primary">Filter</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </form>
             <form method="POST" onsubmit="return verify()">
                 <div class="row">
@@ -83,20 +109,18 @@ mysqli_stmt_close($query);
                             <thead>
                             <tr>
                                 <th scope="col"></th>
-                                <th scope="col">Username</th>
-                                <th scope="col">Email Count</th>
-                                <th scope="col">User Type</th>
-                                <th scope="col">Status</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Manager</th>
+                                <th scope="col">Open Everyday</th>
                             </tr>
                             </thead>
                             <tbody>
-<!--                            --><?php
-//                            foreach ($users as $user) {
-//                                if (($username == '' || $username == null || $username == $user['username']) && ($type == 'all' || $type == null || $type == $user['type']) && ($status == 'all' || $status == null || $status == $user['status'])) {
-//                                    echo '<tr><td><input type="radio" name="username" value="' . $user['username'] . '"></td><td>' . $user['username'] . '</td><td>' . $user['emailCount'] . '</td><td>' . ucfirst($user['type']) . '</td><td>' . ucfirst($user['status']) . '</td></tr>';
-//                                }
-//                            }
-//                            ?>
+                            <?php
+                            foreach ($sites as $s) {
+                                if (($site == 'all' || $site == null || $site == $s['name']) && ($manager == 'all' || $manager == null || $manager == $s['username']) && ($open == 'all' || $open == null || (($open == 'yes') == $s['open'])))
+                                echo '<tr><td><input type="radio" name="site" value="' . $s['name'] . '"></td><td>' . $s['name'] . '</td><td>' . $s['firstName'] . ' ' . $s['lastName'] . '</td><td>' . ($s['open'] ? 'Yes' : 'No') . '</td></tr>';
+                            }
+                            ?>
                             </tbody>
                         </table>
                     </div>
@@ -116,11 +140,6 @@ mysqli_stmt_close($query);
                     </div>
                 </div>
             </form>
-            <?php
-            if (isset($_GET['updated'])) {
-                echo '<div class="alert alert-success text-center" role="alert" style="margin-top: 30px">Updated user '.$_GET['updated'].'.</div>';
-            }
-            ?>
         </div>
     </div>
 </div>
